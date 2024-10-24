5ba0801f3cde0756f811c3d214a8e9519715fc56
iqmost <35768085+iqmost@users.noreply.github.com>
2024-10-24 13:23:56 +0000

    新增Runtime, Memory

---
+++ Array/0001_two-sum.py
@@ -0,0 +1,32 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sortedNums = sorted(nums)
+        a, b = 0, len(sortedNums)-1
+        while a < b:
+            if target < (sortedNums[a]+sortedNums[b]):
+                b-=1
+            elif target > (sortedNums[a]+sortedNums[b]):
+                a+=1
+            else:
+                break
+
+
+        hasA=False
+        for i in range(len(nums)):
+            if nums[i] == sortedNums[a] and not hasA:
+                A=i
+                hasA = True
+            elif nums[i] == sortedNums[b]:
+                B=i
+
+        return A, B
+
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i, j]
+        """
+# 1: Double Looping
+# 2: Data Arranged
+# Memory 92.80%
